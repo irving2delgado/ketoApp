cf320a549f6526c2720346bdc2b0d65ef3d11569
irving2delgado <36970739+irving2delgado@users.noreply.github.com>
2018-07-28 22:43:35 -0600

    edit navbar

--- src/navbar.js
+++ src/navbar.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import './navbar.css';
 
 class NavBar extends Component{
+	static defaultProps = {
+		onNewRecipe() {}
+	  }
+	  
+	  static propTypes = {
+		onNewRecipe: PropTypes.func
+	  }
+	  
 	render() {
 		return (
 			<header>
 				<h2><a>Keto Recipes</a></h2>
 				<nav>
-					<li><a>New Recipe</a></li>
+				<li><a onClick={this.props.onNewRecipe}>New Player</a></li>
 					<li><a>Home</a></li>
 					<li><a>About</a></li>
 					<li><a>Contact Us</a></li>
